62d1e894f63e60cb59b003e1c6de4792b3c71f7a
Becker Leo <hidden>
2020-11-12 00:00:02 +0200

    Viikon 3 loppu tehty

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,7 @@
 package ohtu;
 
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,6 +16,8 @@ public class Tester {
         
         sleep(2);
         
+        // Alkuperäinen testi
+        /*
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
@@ -29,7 +33,58 @@ public class Tester {
         element.submit();
 
         sleep(3);
+        */
+
+        // bullets 2 ja 3
+        /*
+        Random r = new Random();
+        String user = "asfsad"+r.nextInt(12323);
+        WebElement e = driver.findElement(By.linkText("register new user"));
+        e.click();
+
+        sleep(2);
+
+        e = driver.findElement(By.name("username"));
+        e.sendKeys(user);
+        e = driver.findElement(By.name("password"));
+        e.sendKeys(user);
+        e = driver.findElement(By.name("passwordConfirmation"));
+        e.sendKeys(user);
+
+        e = driver.findElement(By.name("signup"));
+        e.submit();
+
+        sleep(2);
+
+        e = driver.findElement(By.linkText("continue to application mainpage"));
+        e.click();
         
+        sleep(2);
+        e = driver.findElement(By.linkText("logout"));
+        e.click();
+
+        sleep(2);
+
+        e = driver.findElement(By.linkText("login"));
+        */
+
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("asd");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("das");
+        element = driver.findElement(By.name("login"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(2);
+        element = driver.findElement(By.name("username"));
+
         driver.quit();
     }
     
